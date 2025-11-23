a700b5e9a888d3135f7488fbb5ab071f3348290b
MomoDjaly <jallil.diab45@gmail.com>
2025-11-23 20:11:08 +0100

    Initial commit - Docker automation script

---
+++ README.md
@@ -0,0 +1,56 @@
+# Docker Stack Automatisation
+
+Ce projet permet d'automatiser la gestion d'une stack **Docker Compose** à l'aide d'un script Bash ('stack.sh').
+
+
+## Fonctionnalités
+Le script permet de :
+
+  - Démarrer la stack
+  - Arrêter la stack
+  - Afficher l'état des conteneurs
+  - Consulter les logs d'un service
+  - Rédémarrer un service spécifique
+
+##Utilisation
+
+Rendre le script éxécutable:
+
+"chmod +x stack.sh"
+
+Puis utiliser l'unde des commandes suivantes :
+
+### Démarrer la stack
+
+"./stack.sh start"
+
+### Arrêter la stack
+
+./stack.sh stop
+
+### Voir les conteneurs actifs
+
+./stack.sh status
+
+### Voir les logs d'un service
+
+./stack.sh logs <service>
+
+Exemple= "./stack.sh logs web"
+
+###Redémarrer un service
+
+./stack.sh restart <service>
+
+
+
+## Préréquis
+
+  - Docker
+  - Docker Compose
+  - Linux/WSL/Ubuntu
+
+## Auteur et but du projet
+
+Mohamed Djalil DIABAGATE Étudiant en BTS SIO
+Projet réalisé dans le cadre d'un apprentissage Docker et Compose
---
+++ stack.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# =====================================
+#  Script de gestion Docker Compose
+#  Auteur : Mohamed Djalil Diabagate
+#  Description :
+#       Automatisation de la stack Docker :
+#       start, stop, status, logs, restart
+# =====================================
+
+#Couleurs (pour afficage stylé)
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+NC="\e[0m" # No Color
+
+#Vérification: un permier argument est obligatoire
+
+if [ -z "$1" ]; then
+  echo -e "${RED} Aucune commande précisée.${NC}"
+  echo "Usage: ./stack.sh <start|stop|status|logs|restart>"
+  exit 1
+fi
+
+case "$1" in
+
+  start)
+    echo -e "${GREEN} Démarrage de la stack ...${NC}"
+    docker-compose up -d
+    ;;
+
+  stop)
+    echo -e "${RED} Arrêt de la stack ...${NC}"
+    docker-compose down
+    ;;
+
+  status)
+    echo -e "${BLUE} Etat des conteneurs :${NC}"
+    docker ps
+    ;;
+
+  logs)
+    if [ -z "$2" ]; then
+      echo -e "${YELLOW} Veuillez préciser un service :${NC}"
+      echo "./stack.sh logs <service>"
+      exit 1
+    fi
+    echo -e "${BLUE} Logs du service : $2${NC}"
+    docker-compose logs
+    ;;
+
+  restart)
+    if [ -z "$2" ]; then
+      echo -e "${YELLOW} Veuillez préciser un service a redémarrer:${NC}"
+      echo "./stack.sh restart <service>"
+      exit 1
+    fi
+    echo -e "${GREEN} Rédemarrage du service: $2${NC}"
+    docker-compose restart $2
+    ;;
+
+  *)
+    echo -e "${YELLOW}Commandes disponibles :${NC}"
+    echo "./stack.sh start"
+    echo "./stack.sh stop"
+    echo "./stach.sh status"
+    echo "./stack.sh logs <service>"
+    echo "./stack.sh restart <service>"
+    exit 1
+   ;;
+esac
